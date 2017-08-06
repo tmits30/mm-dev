772cee0569226467bd349408084a940c555be25a
Takuji Mitsuishi <takuji.mitsuishi@fixstars.com>
2017-08-06 16:45:56 +0900

    Use macro for accessing register

--- test/cpp/mc6502/sim_mpu.cpp
+++ test/cpp/mc6502/sim_mpu.cpp
@@ -30,7 +30,6 @@ public:
 
     using addr_t = vluint16_t;
     using bus_t = vluint8_t;
-    using mem_t = std::tuple<addr_t, bus_t>;
 
     Memory(const std::size_t bits = 8, const std::size_t depth = 0xffff) :
         bits_(bits),
@@ -60,7 +59,7 @@ public:
         std::fill(buf_.get(), buf_.get() + depth_, 0);
     }
 
-    void Set(const std::vector<mem_t>& mem_vec)
+    void Set(const std::vector<std::tuple<addr_t, bus_t>>& mem_vec)
     {
         for (const auto& d : mem_vec) Write(d);
     }
@@ -92,11 +91,17 @@ private:
 
 class TestBench
 {
+#define MPU_TOP(ptr, element) \
+    (ptr)->mpu__DOT__##element
+#define MPU_CONTROLLER(ptr, element) \
+    (ptr)->mpu__DOT__controller__DOT__##element
+#define MPU_DATAPATH(ptr, element) \
+    (ptr)->mpu__DOT__datapath__DOT__##element
+
 public:
 
     using addr_t = Memory::addr_t;
     using bus_t = Memory::bus_t;
-    using mem_t = Memory::mem_t;
 
     const int PERIOD = 1;
 
@@ -124,7 +129,7 @@ public:
     }
 
     void Reset(
-        const std::vector<mem_t>& mem_vec,
+        const std::vector<std::tuple<addr_t, bus_t>>& mem_vec,
         const bus_t a = 0x11,
         const bus_t x = 0x22,
         const bus_t y = 0x33,
@@ -134,6 +139,7 @@ public:
         const addr_t pc = 0x0000)
     {
         // Reset memory
+        mem_->Reset();
         mem_->Set(mem_vec);
 
         // Reset register
@@ -149,8 +155,8 @@ public:
         // Set register
         RES_N(1); CLK(1);
         A(a); X(x); Y(y); S(s); P(p); AB(ab); PC(pc);
-        top_->mpu__DOT__controller__DOT__cur_state = 0;
-        top_->mpu__DOT__controller__DOT__nxt_state = 1;
+        MPU_CONTROLLER(top_, cur_state) = 0;
+        MPU_CONTROLLER(top_, nxt_state) = 1;
     }
 
     void Run(const int cycles)
@@ -179,7 +185,7 @@ public:
     }
 
     bool Verify(
-        const std::vector<mem_t>& mem_vec,
+        const std::vector<std::tuple<addr_t, bus_t>>& mem_vec,
         const bus_t a = 0x11,
         const bus_t x = 0x22,
         const bus_t y = 0x33,
@@ -274,16 +280,16 @@ public:
     bus_t DB_IN() { return top_->DB_IN; }
     bus_t DB_OUT() { return top_->DB_OUT; }
 
-    bus_t A() { return top_->mpu__DOT__datapath__DOT__a; }
-    bus_t X() { return top_->mpu__DOT__datapath__DOT__x; }
-    bus_t Y() { return top_->mpu__DOT__datapath__DOT__y; }
-    bus_t S() { return top_->mpu__DOT__datapath__DOT__s; }
-    bus_t T() { return top_->mpu__DOT__datapath__DOT__t; }
-    bus_t P() { return top_->mpu__DOT__datapath__DOT__p; }
-    bus_t PCL() { return top_->mpu__DOT__datapath__DOT__pcl; }
-    bus_t PCH() { return top_->mpu__DOT__datapath__DOT__pch; }
+    bus_t A() { return MPU_DATAPATH(top_, a); }
+    bus_t X() { return MPU_DATAPATH(top_, x); }
+    bus_t Y() { return MPU_DATAPATH(top_, y); }
+    bus_t S() { return MPU_DATAPATH(top_, s); }
+    bus_t T() { return MPU_DATAPATH(top_, t); }
+    bus_t P() { return MPU_DATAPATH(top_, p); }
+    bus_t PCL() { return MPU_DATAPATH(top_, pcl); }
+    bus_t PCH() { return MPU_DATAPATH(top_, pch); }
     addr_t PC() { return (PCH() << 8) + PCL(); }
-    bus_t IR() { return top_->mpu__DOT__instr; }
+    bus_t IR() { return MPU_TOP(top_, instr); }
 
     const bool CLK() const { return top_->CLK; }
     const bool RES_N() const { return top_->RES_N; }
@@ -296,16 +302,16 @@ public:
     const bus_t DB_IN() const { return top_->DB_IN; }
     const bus_t DB_OUT() const { return top_->DB_OUT; }
 
-    const bus_t A() const { return top_->mpu__DOT__datapath__DOT__a; }
-    const bus_t X() const { return top_->mpu__DOT__datapath__DOT__x; }
-    const bus_t Y() const { return top_->mpu__DOT__datapath__DOT__y; }
-    const bus_t S() const { return top_->mpu__DOT__datapath__DOT__s; }
-    const bus_t T() const { return top_->mpu__DOT__datapath__DOT__t; }
-    const bus_t P() const { return top_->mpu__DOT__datapath__DOT__p; }
-    const bus_t PCL() const { return top_->mpu__DOT__datapath__DOT__pcl; }
-    const bus_t PCH() const { return top_->mpu__DOT__datapath__DOT__pch; }
+    const bus_t A() const { return MPU_DATAPATH(top_, a); }
+    const bus_t X() const { return MPU_DATAPATH(top_, x); }
+    const bus_t Y() const { return MPU_DATAPATH(top_, y); }
+    const bus_t S() const { return MPU_DATAPATH(top_, s); }
+    const bus_t T() const { return MPU_DATAPATH(top_, t); }
+    const bus_t P() const { return MPU_DATAPATH(top_, p); }
+    const bus_t PCL() const { return MPU_DATAPATH(top_, pcl); }
+    const bus_t PCH() const { return MPU_DATAPATH(top_, pch); }
     const addr_t PC() const { return (PCH() << 8) + PCL(); }
-    const bus_t IR() const { return top_->mpu__DOT__instr; }
+    const bus_t IR() const { return MPU_TOP(top_, instr); }
 
     // Setter
     void CLK(const bool wd) { top_->CLK = wd; }
@@ -318,16 +324,16 @@ public:
     void DB_IN(const bus_t wd) { top_->DB_IN = wd; }
     void DB_OUT(const bus_t wd) { top_->DB_OUT = wd; }
 
-    void A(const bus_t wd) { top_->mpu__DOT__datapath__DOT__a = wd; }
-    void X(const bus_t wd) { top_->mpu__DOT__datapath__DOT__x = wd; }
-    void Y(const bus_t wd) { top_->mpu__DOT__datapath__DOT__y = wd; }
-    void S(const bus_t wd) { top_->mpu__DOT__datapath__DOT__s = wd; }
-    void T(const bus_t wd) { top_->mpu__DOT__datapath__DOT__t = wd; }
-    void P(const bus_t wd) { top_->mpu__DOT__datapath__DOT__p = wd; }
-    void PCL(const bus_t wd) { top_->mpu__DOT__datapath__DOT__pcl = wd; }
-    void PCH(const bus_t wd) { top_->mpu__DOT__datapath__DOT__pch = wd; }
+    void A(const bus_t wd) { MPU_DATAPATH(top_, a) = wd; }
+    void X(const bus_t wd) { MPU_DATAPATH(top_, x) = wd; }
+    void Y(const bus_t wd) { MPU_DATAPATH(top_, y) = wd; }
+    void S(const bus_t wd) { MPU_DATAPATH(top_, s) = wd; }
+    void T(const bus_t wd) { MPU_DATAPATH(top_, t) = wd; }
+    void P(const bus_t wd) { MPU_DATAPATH(top_, p) = wd; }
+    void PCL(const bus_t wd) { MPU_DATAPATH(top_, pcl) = wd; }
+    void PCH(const bus_t wd) { MPU_DATAPATH(top_, pch) = wd; }
     void PC(const addr_t wd) { PCL(wd & 0xff); PCH((wd >> 8) & 0xff); }
-    void IR(const bus_t wd) { top_->mpu__DOT__instr = wd; }
+    void IR(const bus_t wd) { MPU_TOP(top_, instr) = wd; }
 
     // Memory Operations
     const bus_t RData(const addr_t ra) const
@@ -350,37 +356,45 @@ private:
     const bool verbose_;
 
     int time_;
+
+#undef MPU_TOP
+#undef MPU_CONTROLLER
+#undef MPU_DATAPATH
 };
 
 
 int main(int argc, char **argv)
 {
+    // Intialize Verilated
     Verilated::commandArgs(argc, argv);
 
-    auto tb = std::make_unique<TestBench>("sim_mpu.vcd");
+    // Create testbench
+    auto tb = std::make_unique<TestBench>("sim_mpu.vcd", false);
 
+    // Read test config yaml file
     const auto test_patterns = YAML::LoadFile("sim_mpu.yml");
 
+    // Load memory vector from yaml file function
     auto load_mem = [](const YAML::Node& mem_node) {
-        std::vector<TestBench::mem_t> ret;
+        std::vector<std::tuple<TestBench::addr_t, TestBench::bus_t>> ret;
         for (const auto& mem : mem_node) {
             const auto addr = mem.first.as<int>();
             const auto data = mem.second.as<int>();
-            ret.emplace_back(TestBench::mem_t(addr, data));
+            ret.emplace_back(
+                std::tuple<TestBench::addr_t, TestBench::bus_t>(addr, data));
         }
         return ret;
     };
 
+    // Run tests from yaml file
     for (const auto& op : test_patterns) {
         const auto op_name = op["name"].as<std::string>();
-        std::cout << op_name << std::endl;
 
         for (const auto& mode : op["mode"]) {
             const auto mode_name = mode["name"].as<std::string>();
-            std::cout << mode_name << std::endl;
 
             // Execute a test pattern
-            const auto input = mode["input"];
+            const auto& input = mode["input"];
             tb->Reset(
                 load_mem(input["mem"]),
                 input["reg"]["A"].as<int>(),
@@ -394,7 +408,7 @@ int main(int argc, char **argv)
             tb->Run(mode["cycle"].as<int>());
 
             // Verify the test
-            const auto expected = mode["expected"];
+            const auto& expected = mode["expected"];
             const auto is_valid = tb->Verify(
                 load_mem(expected["mem"]),
                 expected["reg"]["A"].as<int>(),
@@ -406,7 +420,8 @@ int main(int argc, char **argv)
                 expected["reg"]["PC"].as<int>()
             );
 
-            std::cout << is_valid << std::endl;
+            printf("%s: %s: %s\n", op_name.c_str(), mode_name.c_str(),
+                   is_valid ? "Success" : "Fail");
         }
     }
 
