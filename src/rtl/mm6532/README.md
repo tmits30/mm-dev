be41313d669e4b01027af0346d90c6c3afb5873a
Takuji Mitsuishi <takuji.mitsuishi@fixstars.com>
2017-09-10 23:22:30 +0900

    +README.md

---
+++ src/rtl/mm6532/README.md
@@ -0,0 +1,19 @@
+# 6532 RAM-I/O-Timer (RIOT)
+
+## Features
+
+- 128 x 8 static RAM
+- Two 8 bit bidirectional data ports
+- Programmable interval timer with interrupt capability
+- TTL & CMOS compatible peripheral lines
+- One port has direct transistor drive capability
+- Programmable edge -sensitive interrupt input
+- 8 bit bidirectional data bus
+- 6500/6800 bus compatible
+- 1 MHz and 2 MHz parts available
+- Single +5V power supply
+
+## Reference
+
+- http://archive.6502.org/datasheets/mos_6532_riot.pdf
+- http://generalthomas.com/PET/6532_RIOT.pdf
--- src/rtl/mm6532/mm6532.v
+++ src/rtl/mm6532/mm6532.v
@@ -1,23 +1,4 @@
-/**
- * 6532 RAM-I/O-Timer (RIOT)
- *
- * Features
- * - 128 x 8 static RAM
- * - Two 8 bit bidirectional data ports
- * - Programmable interval timer with interrupt capability
- * - TTL & CMOS compatible peripheral lines
- * - One port has direct transistor drive capability
- * - Programmable edge -sensitive interrupt input
- * - 8 bit bidirectional data bus
- * - 6500/6800 bus compatible
- * - 1 MHz and 2 MHz parts available
- * - Single +5V power supply
- *
- * Reference
- * - http://archive.6502.org/datasheets/mos_6532_riot.pdf
- * - http://generalthomas.com/PET/6532_RIOT.pdf
- */
-
+// 6532 RAM-I/O-Timer (RIOT)
 // TODO: understand edge-detecting
 module mm6532(
   input        CLK,
