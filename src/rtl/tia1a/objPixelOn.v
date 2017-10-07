d702a44bca462146df5aeef3e2ca2ac0519a0444
Takuji Mitsuishi <takuji.mitsuishi@fixstars.com>
2017-10-07 09:56:07 +0900

    Bugfix 2copies wide and 3 copies medium

--- src/rtl/tia1a/objPixelOn.v
+++ src/rtl/tia1a/objPixelOn.v
@@ -18,14 +18,14 @@ module objPixelOn(
 
   always @(*) begin
     case (objSize)
-      3'd0: objSizeOn = (objByteIndex & 9'b00000001) != 0;
-      3'd1: objSizeOn = (objByteIndex & 9'b00000101) != 0;
-      3'd2: objSizeOn = (objByteIndex & 9'b00010001) != 0;
-      3'd3: objSizeOn = (objByteIndex & 9'b00010101) != 0;
-      3'd4: objSizeOn = (objByteIndex & 9'b10000001) != 0;
-      3'd5: objSizeOn = (objByteIndex & 9'b00000011) != 0;
-      3'd6: objSizeOn = (objByteIndex & 9'b10010001) != 0;
-      3'd7: objSizeOn = (objByteIndex & 9'b00001111) != 0;
+      3'd0: objSizeOn = (objByteIndex & 9'b000000001) != 0;
+      3'd1: objSizeOn = (objByteIndex & 9'b000000101) != 0;
+      3'd2: objSizeOn = (objByteIndex & 9'b000010001) != 0;
+      3'd3: objSizeOn = (objByteIndex & 9'b000010101) != 0;
+      3'd4: objSizeOn = (objByteIndex & 9'b100000001) != 0;
+      3'd5: objSizeOn = (objByteIndex & 9'b000000011) != 0;
+      3'd6: objSizeOn = (objByteIndex & 9'b100010001) != 0;
+      3'd7: objSizeOn = (objByteIndex & 9'b000001111) != 0;
     endcase
   end
 
